d1ad5484c98d15b8e76cbbf8d46732ca7c5b783e
KoCa <50935441+CokaVN11@users.noreply.github.com>
2022-06-22 22:17:02 +0700

    Update recv_safe, send_safe

--- client.py
+++ client.py
@@ -4,7 +4,34 @@ import re
 HOST = "127.198.162.1"
 PORT = 52314
 BUZSIZE = 1024
+FORMAT = "utf-8"
 special_char = re.compile("[@_!#$%^&*()<>?/\|}{~:]")
+
+def send_safe(conn: socket.socket(), msg):
+    """
+    If the connection is valid, send the message and wait for a response
+    
+    :param conn: socket.socket()
+    :type conn: socket.socket()
+    :param msg: the message to send
+    """
+    if conn:
+        conn.sendall(msg.encode(FORMAT))
+        conn.recv(BUFSIZE)
+
+def recv_safe(conn: socket.socket()):
+    """
+    It receives a message from the client, sends it back to the client, and returns the message
+    
+    :param conn: socket.socket()
+    :type conn: socket.socket()
+    :return: The message that was sent to the server.
+    """
+    if conn:
+        msg = conn.recv(BUFSIZE).decode(FORMAT)
+        conn.sendall(msg.encode(FORMAT))
+        return msg
+    else: return None
 
 def ShowMenu():
     print("MENU")
@@ -12,10 +39,9 @@ def ShowMenu():
     for i in range(len(task)):
         print(f"{i+1}. {task[i]}")
 
-
-
 def isValidUsername(username: str):
-    valid = True
+    """ Checking whether username is valid or not."""
+    valid: bool = True
     while valid:
         if len(username) < 5:
             valid = False
@@ -35,29 +61,36 @@ def isValidUsername(username: str):
 
 
 def Register():
+    """
+    It asks for a username, password and bank number, then it checks if the username, password, bank number is valid
+    If all of these are true, it sends the username, password and bank number to the server.
+    :return: a boolean value
+    """
     username = input("Username: ")
     password = input("Password: ")
     bank = int(input("Bank number: "))
+    isValidUsername(username)
     while not isValidUsername(username):
         username = input("Pls type 'Username' again: ")
     while not len(password) >= 3:
         password = input("Pls type 'Password' again: ")
     while not 1000000000 <= bank <= 10000000000:
         bank = int(input("Pls type 'Bank number' again: "))
-    client.sendall(username.encode("utf-8"))
-    client.recv(BUZSIZE)
-    client.sendall(password.encode("utf-8"))
-    client.recv(BUZSIZE)
-    client.sendall(str(bank).encode("utf-8"))
-    client.recv(BUZSIZE)
+    send_safe(client, username)
+    send_safe(client, password)
+    send_safe(client, str(bank))
     print("Send finished")
-    rep = client.recv(BUZSIZE).decode("utf-8")
+    rep = recv_safe(client)
     if rep == "Oke":
         return 1
     return 0
     # return (username, password, bank)
 
 def Login():
+    """
+    It asks for a username and password, and then sends them to the server
+    :return: A boolean value
+    """
     username = input("Username: ")
     password = input("Password: ")
     while not isValidUsername(username):
@@ -65,10 +98,8 @@ def Login():
     while not len(password) >= 3:
         password = input("Pls type 'Password' again: ")
 
-    client.sendall(username.encode("utf-8"))
-    client.recv(BUZSIZE)
-    client.sendall(password.encode("utf-8"))
-    client.recv(BUZSIZE)
+    send_safe(client, username)
+    send_safe(client, password)
     print("Send finished")
     rep = client.recv(BUZSIZE).decode("utf-8")
     return rep == "Oke"
--- server.py
+++ server.py
@@ -4,10 +4,49 @@ import string
 
 HOST = "127.198.162.1"
 PORT = 52314
-BUZSIZE = 1024
+BUFSIZE = 1024
+FORMAT = "utf-8"
+
+
+def send_safe(conn: socket.socket(), msg):
+    """Send msg to the host and receive the response
+
+    Args:
+        conn (socket.socket): socket to the host
+        msg (str): message to send
+    """
+    if conn:
+        conn.sendall(msg.encode(FORMAT))
+        conn.recv(BUFSIZE)
+
+
+def recv_safe(conn: socket.socket()):
+    """Receive msg from the host and send back
+
+    Args:
+        conn (socket.socket): socket to the host
+
+    Returns:
+        str or None: message received
+    """
+    if conn:
+        msg = conn.recv(BUFSIZE).decode(FORMAT)
+        conn.sendall(msg.encode(FORMAT))
+        return msg
+    else:
+        return None
 
 
 def isExistTable(sqlConn: sqlite3.Connection, table: str):
+    """
+    It checks if a table exists in a database
+
+    :param sqlConn: sqlite3.Connection
+    :type sqlConn: sqlite3.Connection
+    :param table: the name of the table you want to check
+    :type table: str
+    :return: A boolean value.
+    """
     with sqlConn:
         cursor = sqlConn.cursor()
         # get name of table in database
@@ -26,6 +65,20 @@ def isExistTable(sqlConn: sqlite3.Connection, table: str):
 def insertUserIntoTable(
     sqlConn: sqlite3.Connection, table: str, name: str, password: str, bank: int
 ):
+    """
+    It creates a table if it doesn't exist, then inserts a row into the table
+
+    :param sqlConn: sqlite3.Connection
+    :type sqlConn: sqlite3.Connection
+    :param table: The name of the table you want to insert the user into
+    :type table: str
+    :param name: str
+    :type name: str
+    :param password: str
+    :type password: str
+    :param bank: int
+    :type bank: int
+    """
     with sqlConn:
         cursor = sqlConn.cursor()
         print("Connect successfully")
@@ -47,6 +100,21 @@ def insertUserIntoTable(
 def isNewUser(
     sqlConn: sqlite3.Connection, table: str, username: str, password: str, bank: int
 ):
+    """
+    If the username is not in the table, add it to the table
+
+    :param sqlConn: sqlite3.Connection
+    :type sqlConn: sqlite3.Connection
+    :param table: the table name
+    :type table: str
+    :param username: str
+    :type username: str
+    :param password: str
+    :type password: str
+    :param bank: int
+    :type bank: int
+    :return: A boolean value.
+    """
     with sqlConn:
         cursor = sqlConn.cursor()
         query = "SELECT * FROM " + table + " WHERE username LIKE '" + username + "'"
@@ -61,15 +129,20 @@ def isNewUser(
 
 
 def Register(socConn: socket.socket(), sqlConn: sqlite3.Connection):
-    username = socConn.recv(BUZSIZE)
-    socConn.sendall(username)
-    password = socConn.recv(BUZSIZE)
-    socConn.sendall(password)
-    bank = socConn.recv(BUZSIZE)
-    socConn.sendall(bank)
-    username = username.decode("utf-8")
-    password = password.decode("utf-8")
-    bank = int(bank.decode("utf-8"))
+    """
+    It receives a username, password and bank from the client, and then checks if the user is new or
+    not. If the user is new, it sends "Oke" to the client, otherwise it sends "Not oke".
+
+    :param socConn: socket.socket()
+    :type socConn: socket.socket()
+    :param sqlConn: sqlite3.Connection
+    :type sqlConn: sqlite3.Connection
+    :return: The username, password, and bank.
+    """
+    username = recv_safe(conn)
+    password = recv_safe(conn)
+    bank = int(recv_safe(conn))
+
     print("alo")
     if username and password and bank:
         print("Received")
@@ -81,12 +154,19 @@ def Register(socConn: socket.socket(), sqlConn: sqlite3.Connection):
 
 
 def Login(socConn: socket.socket(), sqlConn: sqlite3.Connection):
-    username = socConn.recv(BUZSIZE)
-    socConn.sendall(username)
-    password = socConn.recv(BUZSIZE)
-    socConn.sendall(password)
-    username = username.decode("utf-8")
-    password = password.decode("utf-8")
+    """
+    It receives a username and password from the client, checks if the username exists in the database,
+    and if it does, it checks if the password is correct. If it is, it sends "Oke" to the client,
+    otherwise it sends "Not oke".
+
+    :param socConn: socket.socket()
+    :type socConn: socket.socket()
+    :param sqlConn: sqlite3.Connection
+    :type sqlConn: sqlite3.Connection
+    :return: The username and password of the user that is logged in.
+    """
+    username = recv_safe(conn)
+    password = recv_safe(conn)
     if username and password:
         print("Received")
         cursor = sqlConn.cursor()
